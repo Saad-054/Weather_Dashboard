0699de70970b639975521255371c52871f2a0306
unknown <saad.siddiqui@msn.com>
2023-08-11 18:42:53 +0100

    function taking from weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,25 @@ $(document).ready(function() {
     const apiKey = "6ef79105ea88baee642e2fc2d962e237";
 }
     // DOM Elements
-    const searchForm = $("#search-form");
+    const searchForm = $("#search-form");;
+    const searchInput = $("#search-input");
+    const historyList = $("#history");
+    const todaySection = $("#today");
+    const forecastSection = $("#forecast");
 
-    
-    
+    // function to obtain weather data from API
+    function fetchWeatherData(city) {
+        const currentApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+        
+
+
+
+    }
+
+
+
+
+
     )
