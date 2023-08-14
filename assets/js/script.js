4b03008f54a008148a00c8efbf15904befd1f4a9
unknown <saad.siddiqui@msn.com>
2023-08-14 17:26:55 +0100

    debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     const apiKey = "6ef79105ea88baee642e2fc2d962e237";
 
     // DOM Elements
-    const searchForm = $("#search-form");;
+    const searchForm = $("#search-form");
     const searchInput = $("#search-input");
     const historyList = $("#history");
     const todaySection = $("#today");
@@ -13,46 +13,55 @@ $(document).ready(function() {
         const currentApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
         const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
 
+        // fu
         $.ajax({
             url: currentApiUrl,
             method: "GET",
         }).then(function(currentResponse) {
+                // fu
             $.ajax({
                 url: forecastApiUrl,
                 method: "GET",
             }).then(function(forecastResponse) {
+                    // fu
                 displayWeatherData(currentResponse, forecastResponse);
             });
         });
     }
 
-    function displayWeatherData(currentData, forecast Data) {
+        // fu
+    function displayWeatherData(currentData, forecastData) {
+            // fu
         const cityName = currentData.name;
         const currentTemperature = currentData.main.temp;
         const currentHumidity = currentData.main.humidity;
-        const currentWindSpped = currentData.wind.speed;
+        const currentWindSpeed = currentData.wind.speed;
 
+            // fu
         todaySection.html(`<h2>${cityName}</h2>
             <p>Current Temperature: ${currentTemperature}°C</p>
             <p>Humidity: ${currentHumidity}%</p>
-            <p>Wind Speed: ${currentWindSpped} m/s</p>`);
+            <p>Wind Speed: ${currentWindSpeed} m/s</p>`);
 
+            // fu
         forecastSection.empty();
-        forecacastData.list.forEach(function(forecacastItem) {
-            const date = forecacastItem.dt_txt.split(" ")[0];
-            const Temperature = forecacastItem.main.temp;
-            const Humidity = forecacastItem.main.humidity;
-            const windSpeed = forecacastItem.main.wind.speed;
+        forecastData.list.forEach(function(forecastItem) {
+            const date = forecastItem.dt_txt.split(" ")[0];
+            const temperature = forecastItem.main.temp;
+            const humidity = forecastItem.main.humidity;
+            const windSpeed = forecastItem.wind.speed;
 
-            const forecacastData = $("<div>").addClass("forecast0card");
-            forecacastCard.html(`<p>Date: ${date}</p>
+            const forecastCard = $("<div>").addClass("forecast-card");
+            forecastCard.html(`<p>Date: ${date}</p>
                 <p>Temperature: ${temperature}°C</p>
                 <p>Humidity: ${humidity}%</p>
                 <p>Wind Speed: ${windSpeed} m/s</p>`);
-            forecastSection.append(forecacastCard);
+
+            forecastSection.append(forecastCard);
         });
     }
 
+        // fu
     searchForm.on("submit", function(event) {
         event.preventDefault();
         const city = searchInput.val().trim();
@@ -63,18 +72,17 @@ $(document).ready(function() {
         }
     });
 
+    
+    
+        // fu
     function addToHistory(city) {
-        const historyItem = $("<Li>").text(city);
+        const historyItem = $("<li>").text(city);
         historyList.prepend(historyItem);
     }
 
-    historyList.on("click", "Li", function() {
-        const selectCity = $(this).text();
+        // fu
+    historyList.on("click", "li", function() {
+        const selectedCity = $(this).text();
         fetchWeatherData(selectedCity);
     });
 });
-
-
-
-
-    )
